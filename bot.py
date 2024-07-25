633f57caffe4968c819b373fb76641e84d2bc1f8
CoolRanger <87192404+CoolRanger@users.noreply.github.com>
2024-07-26 01:15:04 +0800

    added jpgrammar slash command

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import discord
 from discord import app_commands
 import random
+import jpgrammar
 from discord.ext import commands
+from typing import Optional
+from discord.app_commands import Choice
+import asyncio
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -14,6 +18,7 @@ keywords_responses = {
     '狗叫': '汪汪汪',
 }
 
+
 f = open("tk.txt")
 token = f.read()
 f.close
@@ -23,6 +28,10 @@ async def on_ready():
     slash = await bot.tree.sync()
     print(f'Logged in as {bot.user}')
     print(f"Loading {len(slash)} slash commands")
+    jpgrammar.file_process()
+    global jpgrammar_all
+    jpgrammar_all = [jpgrammar.N1, jpgrammar.N2, jpgrammar.N3, jpgrammar.N123]
+    print("已載入日檢文法清單")
 
 @bot.event
 async def on_message(message):
@@ -40,7 +49,6 @@ async def on_message(message):
     await bot.process_commands(message)  # 確保命令仍然可以正常工作
 
 
-# 註冊 slash command
 @bot.tree.command(name="truth")
 async def truth(interaction: discord.Interaction):
     await interaction.response.send_message("Ranger is handsome")
@@ -50,10 +58,64 @@ async def nottruth(interaction: discord.Interaction):
     await interaction.response.send_message("py is handsome")
 
 
+@bot.tree.command(name='japanese_grammar', description='學習日文文法')
+@app_commands.describe(level='級別')
+@app_commands.choices(level=[
+    app_commands.Choice(name='N1', value=0),
+    app_commands.Choice(name='N2', value=1),
+    app_commands.Choice(name='N3', value=2),
+    app_commands.Choice(name='隨機', value=3),
+])
+
+async def jp_grammar(interaction: discord.Interaction, level: app_commands.Choice[int]):
+
+    def check(reaction, user):
+        return user == interaction.user and str(reaction.emoji) in ['✅', '❌'] and reaction.message.id == message.id
+    
+    customer = interaction.user.name
+    lvl = level.value
+    list_name = 'N'+str(lvl+1)
+    rand_list = list(jpgrammar_all[lvl].keys())
+    if lvl == 3:
+        list_name = "隨機"
+    
+    await interaction.response.send_message(f"正在隨機推薦一部{list_name}文法影片給{customer}")
+    message = await interaction.original_response()
+    random_grammar = random.choice(rand_list)
+    await asyncio.sleep(1)
+    await message.edit(content = f'推薦內容{random_grammar}\n請問要換下一個嗎?(請在一分鐘內按表符)')
+    await message.add_reaction('✅')
+    await message.add_reaction('❌')
+
+    
+    
+    while(1):
+        try:
+            reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+            if str(reaction.emoji) == '✅':
+                # 用户点击了打勾，重新推荐
+                await message.clear_reactions()
+                random_grammar = random.choice(rand_list)
+                await message.edit(content=f"正在重新推薦一部{list_name}文法影片給{customer}")
+                await asyncio.sleep(1)
+                await message.edit(content = f'推薦內容{random_grammar}\n請問要換下一個嗎?(請在一分鐘內按表符)')
+                await message.add_reaction('✅')
+                await message.add_reaction('❌')
+            elif str(reaction.emoji) == '❌':
+                # 用户点击了叉叉，输出当前键的值
+                await message.clear_reactions()
+                grammar_value = jpgrammar_all[lvl][random_grammar]
+                await message.edit(content=f'推薦內容: {random_grammar}\n網址: {grammar_value}')
+                break
+        except asyncio.TimeoutError:
+            await message.clear_reactions()
+            await message.edit(content='操作超時。請不要浪費我時間。')
+            break
+    
+
 
 
 @bot.command()
-
 async def yesorno(ctx, *, question=None):
     if question is None:
         await ctx.send('你想問啥?')
--- jpgrammar.py
+++ jpgrammar.py
@@ -1,6 +1,7 @@
 N3 = {}
 N2 = {}
 N1 = {}
+N123 = {}
 f_index = 0
 s_index = 0
 
@@ -13,19 +14,27 @@ def title_process(title):
     title = title[idx:]
 
 def classify(title, link):
-
-
-with open('japanses.txt', 'r', encoding='utf-8') as f:
-    for line in f.readlines():
-        for i in range(len(line)):
-            if line[i]=='【':
-                f_index = i
-            elif line[i]=='h':
-                s_index = i
-                break
-        title = line[f_index:s_index-1]
-        title_process(title)
-        link = line[s_index:]
+    N123[title] = link
+    if title[2] == '３':
         N3[title] = link
-    f.close
-print(N3)
+    elif title[2] == '２':
+        N2[title] = link
+    elif title[2] == '１':
+        N1[title] = link
+
+def file_process():
+    with open('japanses.txt', 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            for i in range(len(line)):
+                if line[i]=='【':
+                    f_index = i
+                elif line[i]=='h':
+                    s_index = i
+                    break
+            title = line[f_index:s_index-1]
+            title_process(title)
+            link = line[s_index:]
+            classify(title, link)
+        f.close
+
+file_process()
